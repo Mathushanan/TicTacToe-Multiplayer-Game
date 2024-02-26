52b57490c6300377816def218b4bd034604c69f2
Mathushanan Mahendrarajah <posttoday0@gmail.com>
2024-02-26 20:00:26 +0530

    Create room & Join room UI + Buttons created

--- lib/screens/main_menu_screen.dart
+++ lib/screens/main_menu_screen.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:tictactoe_game/responsive/responsive.dart';
+import 'package:tictactoe_game/widgets/custom_button.dart';
 
 class MainMenuScreen extends StatelessWidget {
-
-const MainMenuScreen({Key? key}) : super(key: key);
+  const MainMenuScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Main menu'),
-      
-      
-      ),
-    );
+        body: Responsive(
+            child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        CustomButton(
+          onTap: () {},
+          text: "Create Room",
+        ),
+        const SizedBox(height: 20),
+        CustomButton(
+          onTap: () {},
+          text: "Join Room",
+        ),
+      ],
+    )));
   }
 }
