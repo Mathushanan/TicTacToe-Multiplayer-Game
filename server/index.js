741516e03d605477651e9ca7f017ef6a4f731a63
Mathushanan Mahendrarajah <posttoday0@gmail.com>
2024-03-16 19:15:03 +0530

    socket.io package installed and mongo db data base created & configured

--- server/index.js
+++ server/index.js
@@ -1,1 +1,30 @@
-console.log("hey hello");
+const express = require("express");
+const http = require("http");
+const mongoose = require("mongoose");
+
+const app = express();
+const port = process.env.PORT || 3000;
+var server = http.createServer(app);
+
+
+var io = require("socket.io")(server);
+
+// middle ware
+app.use(express.json());
+
+
+const DB =
+  "mongodb+srv://user1:user1@cluster0.m7qff6i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+  mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection successful!");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+server.listen(port, "0.0.0.0", () => {
+    console.log(`Server started and running on port ${port}`);
+  });
