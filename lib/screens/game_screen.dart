0bae1aec097f1fdd5470f5fa880c077f29f5d0bb
Mathushanan Mahendrarajah <posttoday0@gmail.com>
2024-03-28 15:53:26 +0530

    bugs fixed at scoreboard.dart file

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -23,6 +23,7 @@ class _GameScreenState extends State<GameScreen> {
     _socketMethods.updateRoomListener(context);
     _socketMethods.updatePlayersStateListener(context);
     _socketMethods.pointIncreaseListener(context);
+    _socketMethods.endGameListener(context);
   }
 
   @override
